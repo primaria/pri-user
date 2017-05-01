4e03d6311ce3d1da8a9f0a8a000f91fb4529cb40
abenavid <abenavid@vivebuin.cl>
2017-05-01 18:21:53 -0300

    add boooo

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,15 +1,15 @@
 <?php
 
 /*
- * This file is part of the Dektrium project.
+ * This file is part of the abenavid project.
  *
- * (c) Primaria project <http://github.com/primaria/>
+ * (c) abenavid project <http://github.com/abenavid/>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-namespace primaria\user;
+namespace abenavid\user;
 
 use Yii;
 use yii\authclient\Collection;
@@ -18,114 +18,71 @@ use yii\console\Application as ConsoleApplication;
 use yii\i18n\PhpMessageSource;
 
 /**
- * La clase Bootstrap registra el módulo y el componente de aplicación del usuario.
- * También crea algunas reglas de url que se aplicarán
+ * Bootstrap class registers module and user application component. It also creates some url rules which will be applied
  * when UrlManager.enablePrettyUrl is enabled.
  *
- * @author Primaria
+ * @author Dmitry Erofeev <dmeroff@gmail.com>
  */
 class Bootstrap implements BootstrapInterface
 {
     /** @var array Model's map */
+    /** @var Mapa de agreglos del modelo */
     private $_modelMap = [
-        'User'             => 'primaria\user\models\User',
-/*        'Account'          => 'primaria\user\models\Account',
-        'Profile'          => 'primaria\user\models\Profile',
-        'Token'            => 'primaria\user\models\Token',
-        'RegistrationForm' => 'primaria\user\models\RegistrationForm',
-        'ResendForm'       => 'primaria\user\models\ResendForm',
-        'LoginForm'        => 'primaria\user\models\LoginForm',
-        'SettingsForm'     => 'primaria\user\models\SettingsForm',
-        'RecoveryForm'     => 'primaria\user\models\RecoveryForm',
-        'UserSearch'       => 'primaria\user\models\UserSearch',*/
+        'User'              => 'abenavid\user\models\User',
+        'UserQuery'         => 'abenavid\user\models\UserQuery',
+        'SignupForm'        => 'abenavid\user\models\SignupForm',
+        
+        /* 'Account'          => 'abenavid\user\models\Account',
+        'Profile'          => 'abenavid\user\models\Profile',
+        'Token'            => 'abenavid\user\models\Token',
+        'RegistrationForm' => 'abenavid\user\models\RegistrationForm',
+        'ResendForm'       => 'abenavid\user\models\ResendForm',
+        'LoginForm'        => 'abenavid\user\models\LoginForm',
+        'SettingsForm'     => 'abenavid\user\models\SettingsForm',
+        'RecoveryForm'     => 'abenavid\user\models\RecoveryForm',
+        'UserSearch'       => 'abenavid\user\models\UserSearch', */
     ];
 
     /** @inheritdoc */
     public function bootstrap($app)
     {
-        /** @var Module $module */
+        /** @var User $module */
         /** @var \yii\db\ActiveRecord $modelName */
-        if ($app->hasModule('user') && ($module = $app->getModule('user')) instanceof Module) {
+
+        
+        if ( $app->hasModule('user') && ($module = $app->getModule('user')) instanceof User) 
+        {
+           // unifica el mapeo de modulos
             $this->_modelMap = array_merge($this->_modelMap, $module->modelMap);
+
             foreach ($this->_modelMap as $name => $definition) {
-                $class = "primaria\\user\\models\\" . $name;
+                $class = "abenavid\\user\\models\\" . $name;
                 Yii::$container->set($class, $definition);
                 $modelName = is_array($definition) ? $definition['class'] : $definition;
                 $module->modelMap[$name] = $modelName;
                 if (in_array($name, ['User', 'Profile', 'Token', 'Account'])) {
-                    Yii::$container->set($name . 'Query', function () use ($modelName) {
+                    Yii::$container->set($name . 'Query', function () use ($modelName) 
+                    {
                         return $modelName::find();
                     });
                 }
             }
 
-            Yii::$container->setSingleton(Finder::className(), [
-                'userQuery'    => Yii::$container->get('UserQuery'),
-                'profileQuery' => Yii::$container->get('ProfileQuery'),
-                'tokenQuery'   => Yii::$container->get('TokenQuery'),
-                'accountQuery' => Yii::$container->get('AccountQuery'),
+            Yii::$container->set('yii\web\User', [
+                'enableAutoLogin' => true,
+                'loginUrl'        => ['/user/default/login'],
+                'identityClass'   => $module->modelMap['User'],
             ]);
-
-            if ($app instanceof ConsoleApplication) {
-                $module->controllerNamespace = 'primaria\user\commands';
-            } else {
-                Yii::$container->set('yii\web\User', [
-                    'enableAutoLogin' => true,
-                    'loginUrl' => ['/user/login/login'],
-                    'identityClass' => $module->modelMap['User'],
-                ]);
-
-                $configUrlRule = [
-                    'prefix' => $module->urlPrefix,
-                    'rules'  => $module->urlRules,
-                ];
-
-                if ($module->urlPrefix != 'user') {
-                    $configUrlRule['routePrefix'] = 'user';
-                }
-
-                $configUrlRule['class'] = 'yii\web\GroupUrlRule';
-                $rule = Yii::createObject($configUrlRule);
-
-                $app->urlManager->addRules([$rule], false);
-
-                if (!$app->has('authClientCollection')) {
-                    $app->set('authClientCollection', [
-                        'class' => Collection::className(),
-                    ]);
-                }
-            }
-
+          
             if (!isset($app->get('i18n')->translations['user*'])) {
                 $app->get('i18n')->translations['user*'] = [
-                    'class' => PhpMessageSource::className(),
+                    'class'    => PhpMessageSource::className(),
                     'basePath' => __DIR__ . '/messages',
-                    'sourceLanguage' => 'es-ES'
+                    'sourceLanguage' => 'en-US'
                 ];
             }
-
-            Yii::$container->set('primaria\user\Mailer', $module->mailer);
-
-            $module->debug = $this->ensureCorrectDebugSetting();
+            
         }
-    }
-
-    /** Ensure the module is not in DEBUG mode on production environments */
-    public function ensureCorrectDebugSetting()
-    {
-        if (!defined('YII_DEBUG')) {
-            return false;
-        }
-        if (!defined('YII_ENV')) {
-            return false;
-        }
-        if (defined('YII_ENV') && YII_ENV !== 'dev') {
-            return false;
-        }
-        if (defined('YII_DEBUG') && YII_DEBUG !== true) {
-            return false;
-        }
-
-        return Yii::$app->getModule('user')->debug;
+        
     }
 }
