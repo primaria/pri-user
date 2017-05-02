035f35bf942c106bbeeb88a37143fde7165d2d19
abenavid <abenavid@vivebuin.cl>
2017-05-02 15:09:19 -0300

    jj

--- User.php
+++ User.php
@@ -105,7 +105,7 @@ class User extends BaseModule
     /** @var array The rules to be used in URL management. */
     public $urlRules = [
 //         '<id:\d+>'                               => 'profile/show',
-         '<action:(login|logout)>'                => 'manager/<action>',
+//         '<action:(login|logout)>'                => 'manager/<action>',
 //         '<action:(register|resend)>'             => 'registration/<action>',
 //         'confirm/<id:\d+>/<code:[A-Za-z0-9_-]+>' => 'registration/confirm',
 //         'forgot'                                 => 'recovery/request',
