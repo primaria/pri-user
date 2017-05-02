29e0946b0704d56013527df53843750f9669c9bc
abenavid <abenavid@vivebuin.cl>
2017-05-02 15:19:22 -0300

    kjk

--- Bootstrap.php
+++ Bootstrap.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace primaria\user;
+namespace abenavid\user;
 
 use Yii;
 use yii\authclient\Collection;
@@ -21,26 +21,23 @@ use yii\i18n\PhpMessageSource;
  * Bootstrap class registers module and user application component. It also creates some url rules which will be applied
  * when UrlManager.enablePrettyUrl is enabled.
  *
- * @author primaria
+ * @author Dmitry Erofeev <dmeroff@gmail.com>
  */
 class Bootstrap implements BootstrapInterface
 {
     /** @var array Model's map */
     /** @var Mapa de agreglos del modelo */
     private $_modelMap = [
-        'User'              => 'primaria\user\models\User',
-        'UserQuery'         => 'primaria\user\models\UserQuery',
-        'SignupForm'        => 'primaria\user\models\SignupForm',
-        /*'Profile'           => 'primaria\user\models\Profile',
-        'Token'             => 'primaria\user\models\Token',
-        'LoginForm'         => 'primaria\user\models\LoginForm',
-        'Account'           => 'primaria\user\models\Account',
+        'User'              => 'abenavid\user\models\User',
+        'UserQuery'         => 'abenavid\user\models\UserQuery',
+        'SignupForm'        => 'abenavid\user\models\SignupForm',
 
-
-
-
+        /* 'Account'          => 'abenavid\user\models\Account',
+        'Profile'          => 'abenavid\user\models\Profile',
+        'Token'            => 'abenavid\user\models\Token',
+        'RegistrationForm' => 'abenavid\user\models\RegistrationForm',
         'ResendForm'       => 'abenavid\user\models\ResendForm',
-
+        'LoginForm'        => 'abenavid\user\models\LoginForm',
         'SettingsForm'     => 'abenavid\user\models\SettingsForm',
         'RecoveryForm'     => 'abenavid\user\models\RecoveryForm',
         'UserSearch'       => 'abenavid\user\models\UserSearch', */
@@ -55,7 +52,7 @@ class Bootstrap implements BootstrapInterface
 
         if ( $app->hasModule('user') && ($module = $app->getModule('user')) instanceof User)
         {
-           // unifica el mapeo de modulos dentro de un arreglo
+           // unifica el mapeo de modulos
             $this->_modelMap = array_merge($this->_modelMap, $module->modelMap);
 
             foreach ($this->_modelMap as $name => $definition) {
@@ -71,76 +68,21 @@ class Bootstrap implements BootstrapInterface
                 }
             }
 
-            /*Yii::$container->setSingleton(Finder::className(), [
-                'userQuery'    => Yii::$container->get('UserQuery'),
-                'profileQuery' => Yii::$container->get('ProfileQuery'),
-                'tokenQuery'   => Yii::$container->get('TokenQuery'),
-                'accountQuery' => Yii::$container->get('AccountQuery'),
-            ]);*/
-
-            if ($app instanceof ConsoleApplication) {
-                $module->controllerNamespace = 'primaria\user\commands';
-            } else {
-                Yii::$container->set('yii\web\User', [
-                    'enableAutoLogin' => true,
-                    'loginUrl'        => ['/user/manager/login'],
-                    'identityClass'   => $module->modelMap['User'],
-                ]);
-
-                /*$configUrlRule = [
-                    'prefix' => $module->urlPrefix,
-                    'rules'  => $module->urlRules,
-                ];
-
-                if ($module->urlPrefix != 'user') {
-                    $configUrlRule['routePrefix'] = 'user';
-                }
-
-                $configUrlRule['class'] = 'yii\web\GroupUrlRule';
-                $rule = Yii::createObject($configUrlRule);
-
-                $app->urlManager->addRules([$rule], false);
-
-                if (!$app->has('authClientCollection')) {
-                    $app->set('authClientCollection', [
-                        'class' => Collection::className(),
-                    ]);
-                }*/
-            }
+            Yii::$container->set('yii\web\User', [
+                'enableAutoLogin' => true,
+                'loginUrl'        => ['/user/default/login'],
+                'identityClass'   => $module->modelMap['User'],
+            ]);
 
             if (!isset($app->get('i18n')->translations['user*'])) {
                 $app->get('i18n')->translations['user*'] = [
                     'class'    => PhpMessageSource::className(),
                     'basePath' => __DIR__ . '/messages',
-                    'sourceLanguage' => 'es'
+                    'sourceLanguage' => 'en-US'
                 ];
             }
 
-            //Yii::$container->set('dektrium\user\Mailer', $module->mailer);
-
-            $module->debug = $this->ensureCorrectDebugSetting();
-
         }
 
     }
-
-    /** Ensure the module is not in DEBUG mode on production environments */
-    public function ensureCorrectDebugSetting()
-    {
-        if (!defined('YII_DEBUG')) {
-            return false;
-        }
-        if (!defined('YII_ENV')) {
-            return false;
-        }
-        if (defined('YII_ENV') && YII_ENV !== 'dev') {
-            return false;
-        }
-        if (defined('YII_DEBUG') && YII_DEBUG !== true) {
-            return false;
-        }
-
-        return Yii::$app->getModule('user')->debug;
-    }
-
 }
