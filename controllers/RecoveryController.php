5f5b80d5e63d3ddc51df06573e581287ab5c2760
abenavid <abenavid@vivebuin.cl>
2017-05-04 15:53:05 -0300

    ddd

---
+++ controllers/RecoveryController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace primaria\user\controllers;
+
+
+use yii\web\Controller;
+use primaria\user\models\recoveryForm;
+
+class RecoveryController extends Controller
+{
+    /**
+     * Requests password reset.
+     *
+     * @return mixed
+     */
+    public function actionRequestPassword()
+    {
+        $model = new recoveryForm();
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            if ($model->sendEmail()) {
+                Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
+
+                return $this->goHome();
+            } else {
+                Yii::$app->session->setFlash('error', 'Sorry, we are unable to reset password for email provided.');
+            }
+        }
+
+        return $this->render('requestPasswordResetToken', [
+            'model' => $model,
+        ]);
+    }
+
+}
