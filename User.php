ab4e477e308643ed13d83025755c3e6f86f22f6e
abenavid <abenavid@vivebuin.cl>
2017-05-16 15:43:42 -0400

    update

--- User.php
+++ User.php
@@ -13,52 +13,13 @@ class User extends BaseModule
 {
     protected $version = "0.1.0";
 
-    /** Email is changed right after user enter's new email address. */
-    const STRATEGY_INSECURE = 0;
 
-    /** Email is changed after user clicks confirmation link sent to his new email address. */
-    const STRATEGY_DEFAULT = 1;
 
-    /** Email is changed after user clicks both confirmation links sent to his old and new email addresses. */
-    const STRATEGY_SECURE = 2;
-
-    /** @var bool Whether to show flash messages. */
-    public $enableFlashMessages = true;
-
-    /** @var bool Enable the option to register.
-     * True, displays the option to register at the login
-     * false, oculta opcion de registrarse en el inicio de sesion*/
+    /** @var bool Activa la opcion de registro
+     * True, muestra la opcion de registro
+     * false, oculta la opcion de registro*/
     public $enableRegistration = true;
 
-    /** @var bool Whether to remove password field from registration form. */
-    public $enableGeneratingPassword = false;
-
-    /** @var bool Whether user has to confirm his account. */
-    public $enableConfirmation = true;
-
-    /** @var bool Whether to allow logging in without confirmation. */
-    public $enableUnconfirmedLogin = false;
-
-    /** @var bool Whether to enable password recovery. */
-    public $enablePasswordRecovery = true;
-
-    /** @var bool Whether user can remove his account */
-    public $enableAccountDelete = false;
-
-    /** @var int Email changing strategy. */
-    public $emailChangeStrategy = self::STRATEGY_DEFAULT;
-
-    /** @var int The time you want the user will be remembered without asking for credentials. */
-    public $rememberFor = 1209600; // two weeks
-
-    /** @var int The time before a confirmation token becomes invalid. */
-    public $confirmWithin = 86400; // 24 hours
-
-    /** @var int The time before a recovery token becomes invalid. */
-    public $recoverWithin = 21600; // 6 hours
-
-    /** @var int Cost parameter used by the Blowfish hash algorithm. */
-    public $cost = 10;
 
     /** @var array An array of administrator's usernames. */
     public $admins = [];
