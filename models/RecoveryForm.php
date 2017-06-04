784515eabb916ee956e34231908fa7a6d8b8bd2a
abenavid <abenavid@vivebuin.cl>
2017-06-03 23:05:15 -0400

    jmm

---
+++ models/RecoveryForm.php
@@ -0,0 +1,73 @@
+<?php
+namespace primaria\user\models;
+
+use Yii;
+use yii\base\Model;
+use primaria\user\models\User;
+
+/**
+ * Password reset request form
+ */
+class RecoveryForm extends Model
+{
+    public $email;
+
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            ['email', 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'exist',
+                'targetClass' => '\primaria\user\models\User',
+                //'filter' => ['status' => User::STATUS_ACTIVE],
+                'message' => 'There is no user with such email.'
+            ],
+        ];
+    }
+
+    /**
+     * Sends an email with a link, for resetting the password.
+     *
+     * @return bool whether the email was send
+     */
+    public function sendEmail()
+    {
+        /* @var $user User */
+        $user = User::findOne([
+            //'status' => User::STATUS_ACTIVE,
+            'email' => $this->email,
+        ]);
+
+        if (!$user) {
+            return false;
+        }
+
+        if (!User::isPasswordResetTokenValid($user->password_reset_token)) {
+            $user->generatePasswordResetToken();
+            if (!$user->save()) {
+                return false;
+            }
+        }
+
+        echo "<PRE>";
+        print_r(Yii::$app->mailer);
+        echo "</PRE>";
+        die();
+
+        return Yii::$app
+            ->mailer
+            ->compose(
+                ['html' => 'passwordResetToken-html', 'text' => 'passwordResetToken-text'],
+                ['user' => $user]
+            )
+            ->setFrom([Yii::$app->params['supportEmail'] => Yii::$app->name . ' robot'])
+            ->setTo($this->email)
+            ->setSubject('Password reset for ' . Yii::$app->name)
+            ->send();
+    }
+}
