14eafb39426e84f408e1d7d984b4b14e2c59b340
abenavid <abenavid@vivebuin.cl>
2017-06-03 22:38:13 -0400

    add

--- views/recovery/request.php
+++ views/recovery/request.php
@@ -11,22 +11,20 @@ use yii\bootstrap\ActiveForm;
 $this->title = \yii::t('user','Recover your password');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-request-password-reset">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="row">
+    <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title"><?= Html::encode($this->title) ?></h3>
+            </div>
+            <div class="panel-body">
+            	<?php $form = ActiveForm::begin(['id' => 'request-password-form']); ?>
 
-    <p>Please fill out your email. A link to reset password will be sent there.</p>
+                	<?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
+					<?= Html::submitButton(Yii::t('user', 'Continue'), ['class' => 'btn btn-primary btn-block']) ?><br>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'request-password-form']); ?>
-
-                <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton(Yii::t('user', 'Send'), ['class' => 'btn btn-primary']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
-        </div>
-    </div>
+            	<?php ActiveForm::end(); ?>
+        	</div>
+    	</div>
+	</div>
 </div>
