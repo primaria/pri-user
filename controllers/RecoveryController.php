9229e5dc0d86d19666ad7971644b9b8b386e19df
abenavid <abenavid@vivebuin.cl>
2017-06-04 19:12:44 -0400

    pp

--- controllers/RecoveryController.php
+++ controllers/RecoveryController.php
@@ -4,27 +4,56 @@ namespace primaria\user\models;
 
 use Yii;
 use yii\base\Model;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use primaria\user\models\RecoveryForm;
 use primaria\user\mail\sendMail;
 use primaria\user\find\findAuth;
 
 /**
- * Password reset request form
+ * RecoveryController manages password recovery process.
+ *
+ * @property \primaria\user\user $module
+ *
+ * @author Primaria
  */
 class RecoveryForm extends Model
 {
-    const SCENARIO_REQUEST = 'request';
-    const SCENARIO_RESET = 'reset';
 
     /**
-     * @var string
+     * Muestra la página donde el usuario puede solicitar la recuperación de contraseñas.
+     *
+     * @return string
+     * @throws \yii\web\NotFoundHttpException
      */
-    public $email;
+    public function actionRequest()
+    {
+        if (!$this->module->enablePasswordRecovery) {
+            throw new NotFoundHttpException();
+        }
 
-    /**
-     * @var string
-     */
-    public $password;
+        /** @var RecoveryForm $model */
+        $model = \Yii::createObject([
+            'class'    => RecoveryForm::className(),
+            'scenario' => RecoveryForm::SCENARIO_REQUEST,
+        ]);
+        $event = $this->getFormEvent($model);
 
+        $this->performAjaxValidation($model);
+        $this->trigger(self::EVENT_BEFORE_REQUEST, $event);
+
+        if ($model->load(\Yii::$app->request->post()) && $model->sendRecoveryMessage()) {
+            $this->trigger(self::EVENT_AFTER_REQUEST, $event);
+            return $this->render('/message', [
+                'title'  => \Yii::t('user', 'Recovery message sent'),
+                'module' => $this->module,
+            ]);
+        }
+
+        return $this->render('request', [
+            'model' => $model,
+        ]);
+    }
 
 
 }
