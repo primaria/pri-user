67b0a64c1da5e4bc49b8df2cdbd6e059632c0c1e
abenavid <abenavid@vivebuin.cl>
2017-06-06 09:49:06 -0400

    up

--- User.php
+++ User.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the Primaria project
+ * Este archivo es parte de Primaria project
  *
  * (c) Primaria project
  *
@@ -12,59 +12,59 @@ namespace primaria\user;
 use yii\base\Module as BaseModule;
 
 /**
- * This is the main module class for the Yii2-user.
+ * Este es el inicio del modulo pri-user.
  *
  * @property array $modelMap
  *
- * @author Dmitry Erofeev <dmeroff@gmail.com>
+ * @author Primaria TI
  */
 class User extends BaseModule
 {
     const VERSION = '0.1.0';
 
-    /** Email is changed right after user enter's new email address. */
+    /** El Email se cambia justo después de que el usuario ingrese Email */
     const STRATEGY_INSECURE = 0;
 
-    /** Email is changed after user clicks confirmation link sent to his new email address. */
+    /** El email se cambia despues de hacer clic al enlace de confirmacion enviado al nuevo email. */
     const STRATEGY_DEFAULT = 1;
 
-    /** Email is changed after user clicks both confirmation links sent to his old and new email addresses. */
+    /** El email de cambia despues de hacer clic al enlace del correo nuevo y el antiguo */
     const STRATEGY_SECURE = 2;
 
-    /** @var bool Whether to show flash messages. */
+    /** @var bool Si se desea mostrar mensajes flash. */
     public $enableFlashMessages = true;
 
-    /** @var bool Whether to enable registration. */
+    /** @var bool habilita el registro de usuarios. */
     public $enableRegistration = true;
 
-    /** @var bool Whether to remove password field from registration form. */
+    /** @var bool Habilita auto generacion de la password y elimina el campo en el registro. */
     public $enableGeneratingPassword = FALSE;
 
-    /** @var bool Whether user has to confirm his account. */
+    /** @var bool envia confirmacion por email de nuevos usuarios. */
     public $enableConfirmation = true;
 
-    /** @var bool Whether to allow logging in without confirmation. */
+    /** @var bool Permite inicio de session sin confirmacion. */
     public $enableUnconfirmedLogin = false;
 
-    /** @var bool Whether to enable password recovery. */
+    /** @var bool Permite restablecer la password. */
     public $enablePasswordRecovery = true;
 
-    /** @var bool Whether user can remove his account */
+    /** @var bool Permite que el usuario elimine su cuenta */
     public $enableAccountDelete = false;
 
-    /** @var bool Enable the 'impersonate as another user' function */
+    /** @var bool Habilitar la función 'suplantar como otro usuario' */
     public $enableImpersonateUser = true;
 
-    /** @var int Email changing strategy. */
+    /** @var int Estrategia de cambio de correo electrónico. */
     public $emailChangeStrategy = self::STRATEGY_DEFAULT;
 
-    /** @var int The time you want the user will be remembered without asking for credentials. */
+    /** @var int El tiempo que desea que el usuario será recordado. */
     public $rememberFor = 1209600; // two weeks
 
-    /** @var int The time before a confirmation token becomes invalid. */
+    /** @var int El tiempo antes de que un token de confirmación se vuelva inválido. */
     public $confirmWithin = 86400; // 24 hours
 
-    /** @var int The time before a recovery token becomes invalid. */
+    /** @var int El tiempo antes de que un token de recuperacion de password se vuelva inválido. */
     public $recoverWithin = 21600; // 6 hours
 
     /** @var int Cost parameter used by the Blowfish hash algorithm. */
@@ -73,7 +73,7 @@ class User extends BaseModule
     /** @var array An array of administrator's usernames. */
     public $admins = [];
 
-    /** @var string The Administrator permission name. */
+    /** @var string El nombre del permiso de administrador. */
     public $adminPermission;
 
     /** @var array Mailer configuration */
@@ -83,7 +83,7 @@ class User extends BaseModule
     public $modelMap = [];
 
     /**
-     * @var string The prefix for user module URL.
+     * @var string El prefijo para la URL de user Module.
      *
      * @See [[GroupUrlRule::prefix]]
      */
@@ -93,7 +93,7 @@ class User extends BaseModule
      * @var bool Is the user module in DEBUG mode? Will be set to false automatically
      * if the application leaves DEBUG mode.
      */
-    public $debug = false;
+    public $debug = TRUE;
 
     /** @var array The rules to be used in URL management. */
     public $urlRules = [
